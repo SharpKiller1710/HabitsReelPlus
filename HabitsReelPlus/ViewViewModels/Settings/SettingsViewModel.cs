4b24ec4fced996fa87c42cce3fe22108164e01f6
RafiShariar <shariarr767@gmail.com>
2024-04-17 15:53:56 -0500

    Implemetented Payment Method Page

--- HabitsReelPlus/ViewViewModels/Settings/SettingsViewModel.cs
+++ HabitsReelPlus/ViewViewModels/Settings/SettingsViewModel.cs
@@ -1,13 +1,24 @@
 ﻿using HabitsReelPlus.Models;
+using HabitsReelPlus.ViewViewModels.Settings.PaymentMethod;
 using MyFirstMobileApp.ViewViewModels.Base;
+using System.Windows.Input;
 
 namespace HabitsReelPlus.ViewViewModels.Settings
 {
     public class SettingsViewModel : BaseViewModel
     {
+
+        public ICommand OnPaymentClicked { get; }
+
         public SettingsViewModel() 
         {
             Title = TitleSettings.TitleSetting;
+            OnPaymentClicked = new Command(paymentMethodAsync);
+        }
+
+        public async void paymentMethodAsync()
+        {
+            await Application.Current.MainPage.Navigation.PushAsync(new PaymentMethodView());
         }
     }
 }
